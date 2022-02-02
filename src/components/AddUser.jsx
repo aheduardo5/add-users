8f01ca9522ea82ccc050dba6540dd07e8bfd305e
Eduardo Aguilar <aheduardo5@gmail.com>
2022-02-02 13:24:59 -0600

    proyecto guardado

---
+++ src/components/AddUser.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AddUser = () => {
+  return <div>
+      <div className="container-form">
+          <form action="" className="form">
+              <input type="text" placeholder='Add Name'/>
+              <input type="text" placeholder='Add Last Namr'/>
+              <input type="email" placeholder='Add email'/>
+          </form>
+      </div>
+  </div>;
+};
+
+export default AddUser;
